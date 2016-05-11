d8397dfd52e48ea0c8d6120fb55a5f3e24f6510a
Ben <benjohnwie@gmail.com>
2016-05-11 22:45:22 +0200

    more cleanup

--- tmsu.sh
+++ tmsu.sh
@@ -4,15 +4,16 @@ OPEN=xdg-open
 CWD="/"
 TMSU_DB="/path/to/.tmsu/db"
 
-builtin cd $CWD
-
-COUNT=$(tmsu files -D $TMSU_DB --count "$@")
-
 if [ "$#" -le 0 ]; then
     builtin cd $CWD
     tmsu -D $TMSU_DB tags
+    exit;
+fi
 
-elif [[ ${COUNT:0:1} -ne 0 ]]; then
+builtin cd $CWD
+COUNT=$(tmsu files -D $TMSU_DB --count "$@")
+
+if [[ ${COUNT:0:1} -ne 0 ]]; then
     FILES=$(tmsu -D $TMSU_DB files "$@")
     if [[ ! -z $FILES ]]; then
         builtin echo $FILES | tr " " "\n"
