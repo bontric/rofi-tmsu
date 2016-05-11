ec6abf4a3b889d70ae30c014ce25e017f2717e8a
ben <benjohnwie@gmail.com>
2016-05-11 13:36:57 +0200

    initial

---
+++ tmsu.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+OPEN=xdg-open #Open selected Files with this
+CWD="/"
+TMSU_DB="/path/to/.tmsu/db"
+
+# Tags are not allowed to contain a /
+if [[ "$@" == */* ]]; then
+    coproc ( $OPEN $@ )
+    exit;
+fi
+
+# Show all available tags initaly
+if [ "$#" -le 0 ]; then
+    tmsu -D $TMSU_DB tags
+else
+    cd $CWD
+    FILES=$(tmsu -D $TMSU_DB files "$@") ;
+    if [[ ! -z $FILES ]]; then
+        echo $FILES | tr " " "\n"
+    else
+        tmsu -D $TMSU_DB tags
+    fi
+fi
